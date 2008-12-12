545aae68b080ba8b3025308145e46fa59636f1d4
crosset <cyrille.rosset@gmail.com>
2008-12-12 14:00:44 +0000

    add property on version.py

--- healpy/version.py
+++ healpy/version.py
@@ -1,6 +1,6 @@
-rev  = "$Revision: 112 $"
+rev  = "$Revision$"
 
 revnumber = int(rev.strip('$')[9:])
-date = "$Date: 2008-07-20 17:46:55 +0200 (dim, 20 jui 2008) $"
+date = "$Date$"
 
 __version__='trunk-r%d'%(revnumber)
