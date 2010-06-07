eab6045d1024c1dd9a5fa9a17204be37b13655b6
mlejeune <lejeune@apc.univ-paris7.fr>
2010-06-07 12:54:46 +0000

    omp compilation affects spht_lib only

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 
-## use 'gcc_omp' to use openmp
 HEALPIX_TARGET='generic_gcc'
+
+## use 'gcc_omp' to use openmp for sht's
+HEALPIX_TARGET2=HEALPIX_TARGET
 
 from distutils.core import setup, Extension
 from os.path import join,isdir
@@ -40,21 +42,32 @@ healpy_fitsio_src = ['_healpy_fitsio_lib.cc']
 healpix_cxx_dir='hpbeta/%s'%HEALPIX_TARGET
 healpix_cxx_inc = healpix_cxx_dir+'/include'
 healpix_cxx_lib = healpix_cxx_dir+'/lib'
+healpix_cxx_dir2 ='hpbeta/%s'%HEALPIX_TARGET2
+healpix_cxx_inc2 = healpix_cxx_dir2+'/include'
+healpix_cxx_lib2 = healpix_cxx_dir2+'/lib'
 
 if sys.argv[1] != 'sdist':
     compile_healpix_cxx(HEALPIX_TARGET)
-    
     if not ( isdir(healpix_cxx_dir+'/include') and
              isdir(healpix_cxx_dir+'/lib') ):
         raise IOError("No inlcude and lib directory : needed for healpy !")
 
+    if HEALPIX_TARGET2 != HEALPIX_TARGET:
+        compile_healpix_cxx(HEALPIX_TARGET2)
+        if not ( isdir(healpix_cxx_dir2+'/include') and
+                 isdir(healpix_cxx_dir2+'/lib') ):
+            raise IOError("No inlcude and lib directory : needed for healpy !")
+
 ###############################################
 
-healpix_libs=['healpix_cxx','fftpack','cxxsupport','cfitsio']
-healpix_args=[]
-if HEALPIX_TARGET=='gcc_omp':
-    healpix_libs.append('gomp')
-    healpix_args.append('-fopenmp')
+healpix_libs =['healpix_cxx','fftpack','cxxsupport','cfitsio']
+healpix_args =[]
+healpix_libs2=['healpix_cxx','fftpack','cxxsupport','cfitsio']
+healpix_args2=[]
+if HEALPIX_TARGET2=='gcc_omp':
+    healpix_libs2.append('gomp')
+    healpix_args2.append('-fopenmp')
+
 
 #start with base extension
 pixel_lib = Extension('healpy._healpy_pixel_lib',
@@ -68,10 +81,10 @@ pixel_lib = Extension('healpy._healpy_pixel_lib',
 
 spht_lib = Extension('healpy._healpy_sph_transform_lib',
                      sources=[join('healpy','src',s) for s in healpy_spht_src],
-                     include_dirs=[numpy_inc,healpix_cxx_inc],
-                     library_dirs=[healpix_cxx_lib],
-                     libraries=healpix_libs,
-                     extra_compile_args=healpix_args
+                     include_dirs=[numpy_inc,healpix_cxx_inc2],
+                     library_dirs=[healpix_cxx_lib2],
+                     libraries=healpix_libs2,
+                     extra_compile_args=healpix_args2
                      )
 
 hfits_lib = Extension('healpy._healpy_fitsio_lib',
