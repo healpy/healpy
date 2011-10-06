b8a53832b27ae4db5e470759db457d4bce504421
Andrea Zonca <code@andreazonca.com>
2011-10-06 14:52:07 -0700

    removed ext path from conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,6 @@ import sys, os
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 sys.path.append(os.path.abspath('.'))
-sys.path.append(os.path.abspath('./ext'))
 
 # General configuration
 # ---------------------
