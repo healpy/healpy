d25418c284d9cae79abed65ba3c71d576c76d2a6
Cyrille Rosset <cyrille.rosset@gmail.com>
2011-12-05 18:32:34 +0100

    interediate version

--- healpy/src/_ianafast.pyx
+++ healpy/src/_ianafast.pyx
@@ -8,17 +8,30 @@ cimport cython
 
 cdef typedef size_t tsize
 
-cdef extern from "healpix_tables.h":
-    ctypedef enum Healpix_Ordering_Scheme {RING, NESTED}
-
 cdef extern from "arr.h":    
     cdef cppclass arr[T]:
         arr(T *ptr, tsize sz)
 
+cdef extern from "xcomplex.h":
+    cdef cppclass xcomplex[T]:
+       T re, im
+
+cdef extern from "alm.h":
+    cdef cppclass Alm[T]:
+        Alm(int lmax_=0, int mmax_=0)
+
 cdef extern from "healpix_map.h":
+    cdef enum Healpix_Ordering_Scheme:
+        RING, NEST
     cdef cppclass Healpix_Map[T]:
         Healpix_Map()
         Set(arr[T] &data, Healpix_Ordering_Scheme scheme)
 
 cdef extern from "alm_healpix_tools.h":
+    void map2alm_iter(Healpix_Map[T] &m,
+                      Alm[xcomplex[T]] &alm,
+                      int num_iter, arr[double] &weight)
+    
+def ianafast(m, lmax = -1, mmax = -1):
     pass
+
