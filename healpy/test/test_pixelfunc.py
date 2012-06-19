c6c7d91e83d694264c3d32d5627081f564dd60cc
Yu Feng <rainwoodman@gmail.com>
2012-06-19 11:53:02 -0400

    Adding comments to test_pixelfunc.py

--- healpy/test/test_pixelfunc.py
+++ healpy/test/test_pixelfunc.py
@@ -16,26 +16,43 @@ class TestPixelFunc(unittest.TestCase):
         self.assertAlmostEqual(nside2pixarea(512), 3.9947416351188569e-06)
 
     def test_ang2pix_ring(self):
+        # data fixture
         theta0, phi0 = ([ 1.52911759,  0.78550497,  1.57079633,  0.05103658,  3.09055608], 
                       [ 0.        ,  0.78539816,  1.61988371,  0.78539816,  0.78539816])
+        # ensure nside = 1 << 23 is correctly calculated
+        # by comparing the original theta phi are restored.
+        # NOTE: nside needs to be sufficiently large!
         id = ang2pix(1048576 * 8, theta0, phi0, nest=False)
         theta1, phi1 = pix2ang(1048576 * 8, id, nest=False)
         self.assertTrue(numpy.allclose(theta1, theta0))
         self.assertTrue(numpy.allclose(phi1, phi0))
 
+        # ensure nside = 1 << 30 is incorrectly calcualted,
+        # because Healpy_Base2 works upto 1<<29.
+        # Healpy_Base2 shall not crash the test suite
         id = ang2pix(1<<30, theta0, phi0, nest=False)
         theta1, phi1 = pix2ang(1<<30, id, nest=False)
         self.assertFalse(numpy.allclose(theta1, theta0))
         self.assertFalse(numpy.allclose(phi1, phi0))
 
     def test_ang2pix_nest(self):
+        # data fixture
         theta0, phi0 = ([ 1.52911759,  0.78550497,  1.57079633,  0.05103658,  3.09055608], 
                       [ 0.        ,  0.78539816,  1.61988371,  0.78539816,  0.78539816])
+
+        # ensure nside = 1 << 23 is correctly calculated
+        # by comparing the original theta phi are restored.
+        # NOTE: nside needs to be sufficiently large!
+        # NOTE: with Healpy_Base this will fail because nside
+        #       is limited to 1 << 13 with Healpy_Base.
         id = ang2pix(1048576 * 8, theta0, phi0, nest=True)
         theta1, phi1 = pix2ang(1048576 * 8, id, nest=True)
         self.assertTrue(numpy.allclose(theta1, theta0))
         self.assertTrue(numpy.allclose(phi1, phi0))
 
+        # ensure nside = 1 << 30 is incorrectly calcualted,
+        # because Healpy_Base2 works upto 1<<29.
+        # Healpy_Base2 shall not crash the test suite
         id = ang2pix(1<<30, theta0, phi0, nest=True)
         theta1, phi1 = pix2ang(1<<30, id, nest=True)
         self.assertFalse(numpy.allclose(theta1, theta0))
