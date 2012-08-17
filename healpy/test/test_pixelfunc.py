67556d703be6e54df6685fcf9d4e4792268dae64
Cyrille Rosset <cyrille.rosset@gmail.com>
2012-08-17 16:28:06 +0200

    remove top test/ ; all test should be in healpy/test ; add fit_dipole test in test_pixelfunc.py

--- healpy/test/test_pixelfunc.py
+++ healpy/test/test_pixelfunc.py
@@ -1,5 +1,5 @@
 from healpy.pixelfunc import *
-
+import numpy as np
 import unittest
 
 class TestPixelFunc(unittest.TestCase):
@@ -14,6 +14,18 @@ class TestPixelFunc(unittest.TestCase):
 
     def test_nside2pixarea(self):
         self.assertAlmostEqual(nside2pixarea(512), 3.9947416351188569e-06)
+
+    def test_fit_dipole(self):
+        nside = 32
+        npix = nside2npix(nside)
+        d = [0.3, 0.5, 0.2]
+        vec = np.transpose(pix2vec(nside, np.arange(npix)))
+        signal = vec.dot(d)
+        mono, dipole = fit_dipole(signal)
+        self.assertAlmostEqual(mono, 0.)
+        self.assertAlmostEqual(d[0], dipole[0])
+        self.assertAlmostEqual(d[1], dipole[1])
+        self.assertAlmostEqual(d[2], dipole[2])
 
 if __name__ == '__main__':
     unittest.main()
