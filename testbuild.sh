0bda428445d45f0ccac8d98d21b6d970beec5af6
Andrea Zonca <code@andreazonca.com>
2013-03-15 13:11:31 -0700

    updated test build script

--- testbuild.sh
+++ testbuild.sh
@@ -1,4 +1,7 @@
 cd build/lib*/healpy/
-ln -sf ../../../../healpy/test/data test
+for f in wmap_band_iqumap_r9_7yr_V_v4.fits wmap_band_iqumap_r9_7yr_W_v4.fits wmap_temperature_analysis_mask_r9_7yr_v4.fits 
+do
+    ln -sf ../../../../../healpy/test/data/$f test/data/
+done
 nosetests -v
 cd ../../..
