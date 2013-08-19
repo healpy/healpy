b0ff55488c195cdec0ddb2cdad3669808b3b0bf6
Andrea Zonca <code@andreazonca.com>
2013-08-19 12:59:43 -0700

    Raise exception in unit tests setUp
    Raise exception for any type of exception, and print descriptive
    warning on how to get the required data, ancillary to issue #116

--- healpy/test/test_sphtfunc.py
+++ healpy/test/test_sphtfunc.py
@@ -21,13 +21,14 @@ class TestSphtFunc(unittest.TestCase):
             self.map1 = [hp.ma(m) for m in hp.read_map(os.path.join(self.path, 'data', 'wmap_band_iqumap_r9_7yr_W_v4.fits'), (0,1,2))]
             self.map2 = [hp.ma(m) for m in hp.read_map(os.path.join(self.path, 'data', 'wmap_band_iqumap_r9_7yr_V_v4.fits'), (0,1,2))]
             self.mask = hp.read_map(os.path.join(self.path, 'data', 'wmap_temperature_analysis_mask_r9_7yr_v4.fits')).astype(np.bool)
-        except exceptions.IOError:
+        except:
             warnings.warn("""Missing Wmap test maps from the data folder, please download them from Lambda and copy them in the test/data folder:
             http://lambda.gsfc.nasa.gov/data/map/dr4/skymaps/7yr/raw/wmap_band_iqumap_r9_7yr_W_v4.fits
             http://lambda.gsfc.nasa.gov/data/map/dr4/skymaps/7yr/raw/wmap_band_iqumap_r9_7yr_V_v4.fits
             http://lambda.gsfc.nasa.gov/data/map/dr4/ancillary/masks/wmap_temperature_analysis_mask_r9_7yr_v4.fits
             on Mac or Linux you can run the bash script get_wmap_maps.sh from the same folder
             """)
+            raise
         for m in chain(self.map1, self.map2):
             m.mask = np.logical_not(self.mask)
         self.cla = hp.read_cl(os.path.join(self.path, 'data', 'cl_wmap_fortran.fits'))
