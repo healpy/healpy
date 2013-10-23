0cb692c5353419ee422510841e69f738da6550b4
Andrea Zonca <code@andreazonca.com>
2013-10-23 11:02:49 -0700

    run doctests and doctests/cython on travis

--- healpy/run_doctest_cython.py
+++ healpy/run_doctest_cython.py
@@ -1,7 +1,9 @@
 import doctest
+import sys
 
 import _query_disc
-doctest.testmod(_query_disc)
+query_disc_failure_count, query_dist_test_count = doctest.testmod(_query_disc, verbose=True)
 
 import _pixelfunc
-doctest.testmod(_pixelfunc)
+pixelfunc_failure_count, pixelfunc_test_count = doctest.testmod(_pixelfunc, verbose=True)
+sys.exit(query_disc_failure_count + pixelfunc_failure_count)
--- travistest.sh
+++ travistest.sh
@@ -2,5 +2,9 @@
 cd build/lib*/healpy/test/data
 . get_wmap_maps.sh
 cd ../..
-nosetests -v
-exit $?
+nosetests -v --with-doctest
+nosetests_returnvalue=$?
+echo Run Cython extensions doctests
+python run_doctest_cython.py
+cython_doctest_returnvalue=$?
+exit $nosetests_returnvalue + $cython_doctest_returnvalue 
