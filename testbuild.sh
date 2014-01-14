3edfabe5d63c23301383af6ea3f702d2a43f21c3
Andrea Zonca <code@andreazonca.com>
2014-01-14 13:07:16 -0800

    Moved boundaries doctest to unit test suite

--- testbuild.sh
+++ testbuild.sh
@@ -1,9 +1,5 @@
 cd build/lib*/healpy/
-for f in wmap_band_iqumap_r9_7yr_V_v4.fits wmap_band_iqumap_r9_7yr_W_v4.fits wmap_temperature_analysis_mask_r9_7yr_v4.fits 
-do
-    ln -sf ../../../../../healpy/test/data/$f test/data/
-done
-py.test -v --doctest-modules
+py.test -v --doctest-modules --ignore run_doctest_cython.py
 echo Run Cython extensions doctests
 python run_doctest_cython.py
 cd ../../..
