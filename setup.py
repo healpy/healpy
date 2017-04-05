c65c802ff8cfe6fed8713f8cad2760f5a5706f12
Leo Singer <leo.singer@ligo.org>
2017-04-05 11:42:31 -0400

    Add support for Python 3.6

--- setup.py
+++ setup.py
@@ -387,6 +387,7 @@ setup(name='healpy',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
           'Topic :: Scientific/Engineering :: Astronomy',
           'Topic :: Scientific/Engineering :: Visualization'
       ],
