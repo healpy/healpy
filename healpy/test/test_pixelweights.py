deb1e3d0e17b535de6c661b0cb16aa23b993149c
Andrea Zonca <code@andreazonca.com>
2018-05-22 14:01:48 -0700

    Use real-only input alms

--- healpy/test/test_pixelweights.py
+++ healpy/test/test_pixelweights.py
@@ -15,10 +15,10 @@ class TestMap2Alm(unittest.TestCase):
         self.lmax = 96
         alm_size = 4753
         np.random.seed(123)
-        self.input_alm = np.random.normal(size=alm_size) + 1j * np.random.normal(size=alm_size)
+        self.input_alm = np.ones(alm_size, dtype=np.complex)
         self.m = hp.alm2map(self.input_alm, nside=self.nside, lmax=self.lmax)
 
 
     def test_pixelweights(self):
         alm = hp.map2alm(self.m, lmax=self.lmax, use_pixel_weights=True)
-        np.testing.assert_allclose(self.input_alm, alm, rtol=1e-4)
+        np.testing.assert_allclose(self.input_alm, alm, rtol=1e-7)
