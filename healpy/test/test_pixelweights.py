305f4c073a1fa85c66745b878fb2b3d06840f76b
Andrea Zonca <code@andreazonca.com>
2018-05-22 08:00:08 -0700

    map2alm with weights is exact only up to 1.5 nside

--- healpy/test/test_pixelweights.py
+++ healpy/test/test_pixelweights.py
@@ -11,9 +11,9 @@ warnings.filterwarnings('ignore')
 class TestMap2Alm(unittest.TestCase):
 
     def setUp(self):
-        self.lmax = 96
         self.nside = 32
-        self.input_alm = np.ones(4753, dtype=np.complex)
+        self.lmax = 32
+        self.input_alm = np.ones(561, dtype=np.complex)
         self.m = hp.alm2map(self.input_alm, nside=self.nside, lmax=self.lmax)
 
 
