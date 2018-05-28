d29a44b5a74090e80afc2e7a1c33339cc92f78ed
Andrea Zonca <code@andreazonca.com>
2018-05-28 13:42:45 -0700

    Add reference to IDL code
    [skip ci]

--- healpy/test/test_rotator.py
+++ healpy/test/test_rotator.py
@@ -8,6 +8,10 @@ from healpy import Rotator
 path = os.path.dirname( os.path.realpath( __file__ ) )
 
 def test_rotate_map_polarization():
+    """Compare to a rotation from Galactic to Ecliptic of
+    a map of pure Q polarization, the expected value was computed with HEALPix IDL:
+    https://gist.github.com/zonca/401069e1c520e02eaff8cd86149d5900
+    """
     nside = 16
     npix = hp.nside2npix(nside)
     QU_gal = [np.ones(npix, dtype=np.double), np.zeros(npix, dtype=np.double)]
