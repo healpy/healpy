00f9931bc39d5ac2f004b708c06f89a08d6a6e69
Andrea Zonca <code@andreazonca.com>
2021-04-15 18:13:24 -0700

    add nbsphinx extension

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ sys.path.append(os.path.abspath("."))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 
-extensions = ["sphinx.ext.autosummary", "sphinx.ext.autodoc", "numpydoc"]
+extensions = ["sphinx.ext.autosummary", "sphinx.ext.autodoc", "numpydoc", "nbsphinx"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["templates/"]
