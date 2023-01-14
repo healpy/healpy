07de54560ea35893d32e516d05a6495d07fc2bd5
Andrea Zonca <code@andreazonca.com>
2023-01-13 17:45:54 -0800

    Add healpy-specific deprecation warning and decorators
    Adapted from gammapy, closes #806

---
+++ healpy/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Utility functions and classes.
+
+You have to import sub-modules of `healpy.utils` directly,
+the `healpy.utils` namespace is empty.
+
+Examples::
+    from gammapy.utils.deprecation import HealpyDeprecationWarning
+"""
