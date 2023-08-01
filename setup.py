e966e0596ee186bf79b093b6b1dd7eb553baad12
Leo Singer <leo.singer@ligo.org>
2023-08-01 08:51:57 -0400

    Move package metadata to pyproject.toml
    This has several advantages:
    - The project metadata is now stored as configuration, not code.
    - We don't need to explicitly read the README file.
    - The healpy/version.py file is now generated automatically.

--- setup.py
+++ setup.py
@@ -312,36 +312,7 @@ class custom_build_ext(build_ext):
         build_ext.run(self)
 
 
-exec(open("healpy/version.py").read())
-
-
-def readme():
-    with open("README.rst") as f:
-        return f.read()
-
-
 setup(
-    name="healpy",
-    version=__version__,
-    description="Healpix tools package for Python",
-    long_description=readme(),
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
-        "Operating System :: POSIX",
-        "Programming Language :: C++",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Topic :: Scientific/Engineering :: Astronomy",
-        "Topic :: Scientific/Engineering :: Visualization",
-    ],
-    author="C. Rosset, A. Zonca",
-    author_email="cyrille.rosset@apc.univ-paris-diderot.fr",
-    url="http://github.com/healpy",
     packages=["healpy", "healpy.test"],
     libraries=[
         (
@@ -452,10 +423,5 @@ setup(
             "test/data/*.sh",
         ]
     },
-    install_requires=["matplotlib", "numpy>=1.13", "astropy", "scipy"],
-    tests_require=["pytest", "pytest-cython", "pytest-doctestplus", "requests"],
-    test_suite="healpy",
-    license="GPLv2",
     scripts=["bin/healpy_get_wmap_maps.sh"],
-    python_requires=">=3.8",
 )
