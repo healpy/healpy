62c91e44516d17cc0a8abab9a2472a6e97fb9ae2
Leo Singer <leo.singer@ligo.org>
2024-12-05 14:07:36 -0500

    Don't raise KeyError if unrelated modules are not installed

--- lib/healpy/conftest.py
+++ lib/healpy/conftest.py
@@ -8,8 +8,8 @@ def pytest_configure(config):
     try:
         from pytest_astropy_header.display import PYTEST_HEADER_MODULES
 
-        PYTEST_HEADER_MODULES.pop("Pandas")
-        PYTEST_HEADER_MODULES.pop("h5py")
+        PYTEST_HEADER_MODULES.pop("Pandas", None)
+        PYTEST_HEADER_MODULES.pop("h5py", None)
         PYTEST_HEADER_MODULES["astropy"] = "astropy"
         PYTEST_HEADER_MODULES["cython"] = "cython"
     except ImportError:
