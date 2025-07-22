ae6983743423817d3a473db8783229c629895ce5
Joe Zuntz <joezuntz@googlemail.com>
2025-07-22 10:24:42 +0100

    Add unit test for lonra/latra

--- test/test_newvisufunc_example.py
+++ test/test_newvisufunc_example.py
@@ -179,6 +179,16 @@ def test_projview_cart_graticule_vertical_cbar(map_data):
         projection_type="cart",
     )
 
+def test_projview_cart_lonra_latra(map_data):
+    projview(
+        map_data,
+        coord=["G"],
+        projection_type="cart",
+        lonra=[-30, 30],
+        latra=[-30, 30],
+)
+
+
 
 def test_projview_3d(map_data):
     projview(
